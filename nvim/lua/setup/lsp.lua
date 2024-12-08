00079c9aa938768d037e65cfa939be5e17fbe03e
Bruce Cosden <bcosden@Mac.saros.dev>
2024-12-08 16:34:06 -0500

    first commit

---
+++ nvim/lua/setup/lsp.lua
@@ -0,0 +1,126 @@
+-- LSP this is needed for LSP completions in CSS along with the snippets plugin
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  },
+}
+
+-- Give me rounded borders everywhere
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = "rounded"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+--Enable (broadcasting) snippet capability for completion
+-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- LSP Server config
+require("lspconfig").cssls.setup({
+  capabilities = capabilities,
+  settings = {
+    css = {
+      lint = {
+        emptyRules = "ignore",
+      },
+    },
+    scss = {
+      lint = {
+        idSelector = "warning",
+        zeroUnits = "warning",
+        duplicateProperties = "warning",
+        emptyRules = nil,
+      },
+      completion = {
+        completePropertyWithSemicolon = true,
+        triggerPropertyValueCompletion = true,
+      },
+    },
+  },
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = false
+  end,
+})
+require("lspconfig").ts_ls.setup({
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = false
+  end,
+})
+
+require("lspconfig").html.setup({
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = false
+  end,
+})
+
+require("lspconfig").stylelint_lsp.setup({
+  filetypes = { "css", "scss" },
+  root_dir = require("lspconfig").util.root_pattern("package.json", ".git"),
+  settings = {
+    stylelintplus = {
+      -- see available options in stylelint-lsp documentation
+    },
+  },
+  on_attach = function(client)
+    client.server_capabilities.document_formatting = false
+  end,
+})
+
+require'lspconfig'.lua_ls.setup({
+on_init = function(client)
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+})
+
+require'lspconfig'.pyright.setup({})
+
+require'lspconfig'.bashls.setup({})
+
+-- LSP Prevents inline buffer annotations
+vim.diagnostic.open_float()
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_on_insert = false,
+})
+
+local signs = {
+  Error = "󰅚 ",
+  Warn = "󰳦 ",
+  Hint = "󱡄 ",
+  Info = " ",
+}
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = nil })
+end
